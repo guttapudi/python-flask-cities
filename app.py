c3a7f45c5876a08ca05ae410062a0716e820ffba
karthikguttapudi <karthikg@sg.ibm.com>
2022-02-09 14:08:51 +0800

    one

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+cities = [
+  {'city': 'Tokyo', 'population': 37435191},
+  {'city': 'Delhi', 'population': 29399141},
+  {'city': 'Shanghai', 'population': 26317104},
+  {'city': 'Sao Paulo', 'population': 21846507},
+  {'city': 'Mexico City', 'population': 21671908}
+]
+
+@app.route('/cities')
+def get_cities():
+  return jsonify(cities)
+
+port = os.getenv('PORT', '8000')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(port))
